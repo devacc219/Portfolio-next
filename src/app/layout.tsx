5821eeaa8dc926e3bdc35bf8cc0c6e9af24cfc92
Vinay Sahu <devacc219@gmail.com>
2024-10-15 03:40:51 +0530

    children props fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,9 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
       <body>
-        <RootLayoutSplash children={children} />
+        <RootLayoutSplash>
+          {children}
+        </RootLayoutSplash>
       </body>
     </html>
   );
