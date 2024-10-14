3895328628d03e441bf658bbd3b6e169232b294f
Vinay Sahu <devacc219@gmail.com>
2024-10-14 12:35:01 +0530

    splash Screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Image from "next/image";
-import Splash from "./_components/splash";
+import Splash from "./_components/splash/splash";
 import { useState } from "react";
 
 export default function Home() {
