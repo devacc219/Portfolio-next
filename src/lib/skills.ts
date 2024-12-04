a99a2ec708aeaab2412693c88ec3ffa9dfc9d62b
Vinay Sahu <devacc219@gmail.com>
2024-12-04 15:10:52 +0530

    cosmetics

--- src/lib/skills.ts
+++ src/lib/skills.ts
@@ -1,3 +1,5 @@
+import { faAngular, faCss3Alt, faHtml5, faReact } from "@fortawesome/free-brands-svg-icons";
+
 export const skills = {
     title: "<Skills>",
     description: "Skills I possess as a specialist",
@@ -6,16 +8,14 @@ export const skills = {
         "Database development",
         "TODO :: Add skills here"
     ],
-    icons: [
-        "/placeholder.svg?height=50&width=50",
-        "/placeholder.svg?height=50&width=50",
-        "/placeholder.svg?height=50&width=50",
-        "/placeholder.svg?height=50&width=50",
-        "/placeholder.svg?height=50&width=50",
-        "/placeholder.svg?height=50&width=50",
-        "/placeholder.svg?height=50&width=50",
-        "/placeholder.svg?height=50&width=50",
-        "/placeholder.svg?height=50&width=50",
-        "/placeholder.svg?height=50&width=50",
+    // icons: [
+    //     "/placeholder.svg?height=50&width=50",
+    //     "/placeholder.svg?height=50&width=50",
+    // ]
+    icons:[
+        faAngular,
+        faReact,
+        faHtml5,
+        faCss3Alt
     ]
 }
